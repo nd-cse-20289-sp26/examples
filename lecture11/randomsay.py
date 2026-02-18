ebb2f95f5511b70a3a6b3813e617e7b6dee38af6
Peter Bui <pbui@nd.edu>
2026-02-17 21:37:52 -0500

    Lecture 11: update randomsay

--- lecture11/randomsay.py
+++ lecture11/randomsay.py
@@ -1,27 +1,27 @@
 #!/usr/bin/env python3
 
 import os
-import random                                   # Discuss: random module
+import random                          # Discuss: random module
 import sys
 
 # Constants
-                                                # Discuss: set data structure
-NSFW = {'bong', 'sodomized', 'kiss', 'head-in', 'telebears'}
+                                        # Discuss: set data structure
+FORBIDDEN = {'bong', 'sodomized', 'kiss', 'head-in', 'satanic', 'telebears'}
 
 # Main Execution
 
 def main():
-    characters = []                             # Discuss: os.popen
+    cow = []                            # Discuss: os.popen
     for index, line in enumerate(os.popen('cowsay -l')):
-        if not index:                           # Discuss: enumerate
+        if not index:                   # Discuss: enumerate
             continue
 
-        for character in line.split():          # Review: str.split
-            if character not in NSFW:           # Review: searching collection
-                characters.append(character)    # Review: list.append
+        for cow in line.split():        # Review: str.split
+            if cow not in FORBIDDEN:    # Review: searching collection
+                cow.append(cow)         # Review: list.append
     
-    selected = random.choice(characters)
-    os.system(f'cowsay -f {selected}')          # Variant: check exist status
+    selected = random.choice(cow)
+    os.system(f'cowsay -f {selected}')  # Variant: check exist status
 
 if __name__ == '__main__':
     main()
